2422fa043734448d47fd90b77158eed4b02adeaf
Yamato Sasaki <yamatosasaki14@gmail.com>
2019-12-20 14:18:17 -0800

    Account types should be created with seed.rb
    Account Types (Assets, Liabilities, Equity, Revenues, and Expenses) will never change. Therefore, instead of having a creation page,
    one should simply populate the database with seeds.rb with appropriate entries

--- app/controllers/account_types_controller.rb
+++ app/controllers/account_types_controller.rb
@@ -1,16 +1,4 @@
 class AccountTypesController < ApplicationController
-    def new
-        @account_type = AccountType.new
-    end
-
-    def create
-        @account_type = AccountType.new(account_type_params)
-        if @account_type.save
-            redirect_to '/welcome/index'
-        else
-            render 'new'
-        end
-    end
 
     private
     def account_type_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+AccountType.create([{name: "ASSETS", debit?: true}, {name: "LIABILITIES", debit?: false},
+    {name: "EQUITY", debit?: false}, {name: "REVENUES", debit?: false}, {name: "EXPENSES", debit?:true}])
